593c7e93dd9f34dd29f7103fa43cf60dc94ba060
kroogucf <dylankrueger@Knights.ucf.edu>
2021-04-30 04:06:18 -0400

    April 30, 2021 4:06 AM

--- components/JournalMain.js
+++ components/JournalMain.js
@@ -2,5 +2,14 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, Button, View} from "react-native";
 
 export default function JournalMain(){
-    
+    return(<SafeAreaView style={styles.container}>
+        <Text style={styles.title}>JOURNAL APP</Text>
+        <FlatList
+          data={data} 
+          renderItem={({ item }) => (
+            <AllTodos item={item} handleToggle={()=>{handleToggle(item.key)}} todos={data} />
+          )}>
+        </FlatList>
+        <NewTodo submitNewTodo={submitNewTodo}></NewTodo>
+      </SafeAreaView>);
 }
