684f03bf8315e3ee6d3baa5b07758d0b96a2af51
kroogucf <dylankrueger@Knights.ucf.edu>
2021-04-30 04:08:25 -0400

    April 30, 2021 4:08 AM

--- components/JournalMain.js
+++ components/JournalMain.js
@@ -2,14 +2,24 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, TextInput, Button, View} from "react-native";
 
 export default function JournalMain(){
+
+    const submitNewJournal = (text, date) => {
+        setNewJournal((prevJournals) => {
+          return [
+            { key: Math.random().toString(), text: text, date: date, title: title},
+            ...prevJournals
+          ]
+        })
+      }
+
     return(<SafeAreaView style={styles.container}>
         <Text style={styles.title}>JOURNAL APP</Text>
         <FlatList
           data={data} 
           renderItem={({ item }) => (
-            <AllTodos item={item} handleToggle={()=>{handleToggle(item.key)}} todos={data} />
+            <AllJournals item={item} handleToggle={()=>{handleToggle(item.key)}} journals={data} />
           )}>
         </FlatList>
-        <NewTodo submitNewTodo={submitNewTodo}></NewTodo>
+        <NewTodo submitNewJournal={submitNewJournal}></NewTodo>
       </SafeAreaView>);
 }
