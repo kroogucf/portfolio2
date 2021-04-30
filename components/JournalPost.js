3be044d1cbc560ce55a642a5bcaffd48587661aa
kroogucf <dylankrueger@Knights.ucf.edu>
2021-04-30 05:20:40 -0400

    April 30, 2021 5:20 AM

--- components/JournalPost.js
+++ components/JournalPost.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         padding: 5,
         borderBottomWidth: 1,
-        backgroundColor: '#86839B',
+        backgroundColor: '#9BCAF2',
         borderBottomColor: 'black',
         borderRadius: 10,
     },
