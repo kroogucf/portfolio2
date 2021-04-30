c499583c8e8370f2bbb3d573c584010218d39ad7
kroogucf <dylankrueger@Knights.ucf.edu>
2021-04-30 02:18:01 -0400

    April 30, 2021 2:18 AM

--- components/LoginApp.js
+++ components/LoginApp.js
@@ -1,11 +1,35 @@
 import { Button, TextInput, View, Text, StyleSheet } from "react-native";
 import React from "react";
-import { useState, useRef } from "react";
+import { useState } from "react";
+import { useRef } from "react";
 
 export default function LoginApp() {
     let doLogin = () => {
-        if (!useRef.current.value) {
-            setErrorMessage("You left username blacnk")
+        if (!userRef.current.value) {
+            setErrorMessage("You left username blank")
+            return
+        } else {
+
+        }
+
+        if (!passRef.current.value) {
+            setErrorMessage("You left username blank")
+        } else {
+
         }
     }
+
+    let [loggedIn,setLoggedIn] = useState(false)
+    let [username, setUsername] = useState("")
+    let [errorMessage, setErrorMessage] = useState("")
+    let passRef = useRef(null)
+    let userRef = useRef(null)
+
+    let notLoggedInScreen = (
+        <View>
+            <Text>Please Login</Text>
+            <TextInput ref={userRef} placeholder="Username"></TextInput>
+            <TextInput ref={passRef} placeholder="Username"></TextInput>
+        </View>
+    )
 }
