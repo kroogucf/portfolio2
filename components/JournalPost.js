a1f959cbc06139561b6e1fbd0b3af39445e40984
kroogucf <dylankrueger@Knights.ucf.edu>
2021-04-30 04:09:21 -0400

    April 30, 2021 4:09 AM

--- components/JournalPost.js
+++ components/JournalPost.js
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, TextInput, Button, View} from "react-native";
 
-export default function JournalPost() {
+export default function JournalPost(props) {
+
+    let [title, setTitle] = useState(props.title);
+    let [date, setDate] = useState(props.date);
+    let [text, setText] = useState(props.text);
+
+    return(
+        <View>
+            <Text>{title}</Text>
+            <Text>{date}</Text>
+            <Text>{text}</Text>
+        </View>
+    )
 
 }
