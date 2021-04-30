a0701b8b63c4982cf89496ee2a0ca852156ec868
kroogucf <dylankrueger@Knights.ucf.edu>
2021-04-30 03:18:45 -0400

    April 30, 2021 3:18 AM

--- components/Journal.js
+++ components/Journal.js
@@ -1,1 +1,29 @@
-import React from "react"; 
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, Button, View} from "react-native";
+
+export default function JournalApp({ submitJournalPost }) {
+    
+
+    let [text, setNewText] = useState('');
+    let [date, setNewDate] = useState('');
+
+    const newJournalPost = (value) => {
+        setNewText(value);
+    }
+
+    return(
+        <View>
+            <TextInput>
+                placeholder="Name This Post"
+                onChangeText={newJournalPost}
+            </TextInput>
+            <TextInput>
+                placeholder="Date"
+                onChangeText={setNewDate}
+            </TextInput>
+            <Button>
+                onPress
+            </Button>
+        </View>
+    )
+}
