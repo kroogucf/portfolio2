66bf96533f128428c0cf2940782b6389737c83db
kroogucf <dylankrueger@Knights.ucf.edu>
2021-05-04 17:16:03 -0400

    May 4, 2021 5:16 PM

--- components/LoginApp.js
+++ components/LoginApp.js
@@ -6,7 +6,6 @@ import { useRef } from "react";
 // This code is modified from the week 10 development assignment that asked to create a login page with a specific password.
 // I changed the 'Ch@rge!' password part to accept any password and username combination to login as long as something was entered.
 // I also added some styling to the welcome and username message once logged in.
-// I also added logout functionality
 
 export default function LoginApp() {
     const styles = StyleSheet.create({
@@ -57,14 +56,6 @@ export default function LoginApp() {
 
     }
 
-    let doLogout = () => {
-        if (typeof userRef.current === 'object') {
-            console.log("not logged in")
-        }
-        if (typeof userRef.current === 'string') {
-            setLoggedIn(false)
-        }
-    }
 
     let [loggedIn,setLoggedIn] = useState(false)
     let [username, setUsername] = useState("")
@@ -85,7 +76,6 @@ export default function LoginApp() {
             <Text style={styles.welcomeText}>Welcome,</Text>
             <Text style={styles.usernameText}>{username}.</Text>
             <Text>You can view and add to your todo sheet or access your personalized journal below.</Text>
-            <Button onPress={doLogout} title="Logout"></Button>
         </View>
     )
     return !loggedIn ? notLoggedInScreen : loggedInScreen
